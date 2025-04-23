8cad974d63ff4b0037793e3f9c67cd85f5e0b5a3
Shreesha99 <shreeshavenkatram99@gmail.com>
2025-04-23 11:38:13 +0530

    fix: update dependencies in package.json and add new icon showcase component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ButtonShowcaseComponent } from './showcase/button/button-showcase.compo
 import { InputShowcaseComponent } from './showcase/input/input-showcase.component';
 import { AccordionShowcaseComponent } from './showcase/accordion/accordion-showcase.component';
 import { ErrorComponent } from './showcase/error/error.component';
+import { IconShowcaseComponent } from './showcase/icon/icon-showcase.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'accordion', component: AccordionShowcaseComponent },
   { path: 'button', component: ButtonShowcaseComponent },
   { path: 'input', component: InputShowcaseComponent },
+  { path: 'icon', component: IconShowcaseComponent },
   { path: '**', component: ErrorComponent },
 ];
---
+++ src/app/showcase/icon/icon-showcase.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabsComponent } from '../../tabs/tabs.component';
+import { CopyButtonComponent } from '../../copy-button/copy-button.component';
+import { DrIconComponent } from 'drux-ui-angular';
+
+@Component({
+  selector: 'app-icon-showcase',
+  standalone: true,
+  imports: [CommonModule, TabsComponent, CopyButtonComponent, DrIconComponent],
+  templateUrl: './icon-showcase.component.html',
+  styleUrls: ['./icon-showcase.component.scss'],
+})
+export class IconShowcaseComponent {
+  activeTab = 'preview';
+
+  tabs = [
+    { label: 'Preview', value: 'preview' },
+    { label: 'Code', value: 'code' },
+    { label: 'Variants', value: 'variants' },
+  ];
+
+  iconCode = `<dr-icon name="check-circle" size="lg" color="success"></dr-icon>`;
+}
